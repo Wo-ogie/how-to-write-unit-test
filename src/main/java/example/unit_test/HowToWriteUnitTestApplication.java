eada4167eb23bccbf4bcbb3062b86c3aaf0c39fe
Wo-ogie <siwall0105@gmail.com>
2024-04-01 18:05:53 +0900

    프로젝트 생성

---
+++ src/main/java/example/unit_test/HowToWriteUnitTestApplication.java
@@ -0,0 +1,13 @@
+package example.unit_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToWriteUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowToWriteUnitTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/unit_test/HowToWriteUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package example.unit_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowToWriteUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
